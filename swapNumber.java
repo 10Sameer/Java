d204c8a849e9430aae78eca4b377c09ec972a4ee
Sameer <bhandarisameer173@gmail.com>
2025-02-21 20:58:58 +0545

    swap

---
+++ swapNumber.java
@@ -0,0 +1,16 @@
+//swap a number 
+package np.edu.scst.lab1;
+
+public class swapNumber {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        x= x+y;
+        y= x-y;
+        x= x-y;
+        System.out.println("After Swaping, The value of a= "+ x + " " +"and The value of b= " + y);
+    }
+    
+}
+//output: after swap, a= 20 and b= 10
