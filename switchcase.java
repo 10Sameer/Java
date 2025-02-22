0ee7190343733971eab74c899dd0497be2e0e6ba
Sameer <bhandarisameer173@gmail.com>
2025-02-22 20:37:35 +0545

    SwitchCase

---
+++ switchcase.java
@@ -0,0 +1,16 @@
+public class switchcase {
+    public static void main(String[] args) {
+        int a = 1;
+        switch (a){
+            case 1:
+                System.out.println("true");
+                break;
+            case 2:
+                System.out.println("false");
+                break;
+            default: System.out.println("false");
+        }
+    }
+    
+          
+}
