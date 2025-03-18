1dfb058dcc84c2545f30fbcad83414f1cb889ede
Sameer <bhandarisameer173@gmail.com>
2025-03-18 21:30:28 +0545

    multiplication  Of matrix

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -33,4 +33,24 @@ public class MatrixMultiplication {
             }
         }
 
-      
+        // Matrix multiplication
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                for (int k = 0; k < cols1; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        // Display result matrix
+        System.out.println("Product of matrices:");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
