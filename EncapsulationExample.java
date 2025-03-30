6c8d04e089c51068f2fd73b9fbcc6edd765b7ec9
Sameer <bhandarisameer173@gmail.com>
2025-03-30 21:46:56 +0545

    Polymorphism

---
+++ EncapsulationExample.java
@@ -0,0 +1,47 @@
+public class EncapsulationExample {
+    
+}
+// Encapsulated class
+class Student {
+    // Private fields (data hiding)
+    private String name;
+    private int age;
+    private String grade;
+
+    // Constructor
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    // Getter method for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter method for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter method for age (validating age)
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Invalid age!");
+        }
+    }
+
+    // Getter method for grade
+    public String getGrade() {
+        return grade;
+    }
+
+
