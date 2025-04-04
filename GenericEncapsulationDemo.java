e4bd7f3742621e7c99e0715748f3e4dc176eace1
Sameer <bhandarisameer173@gmail.com>
2025-04-04 21:56:47 +0545

    aaa

--- GenericEncapsulationDemo.java
+++ GenericEncapsulationDemo.java
@@ -19,4 +19,22 @@ class DataHolder<T> {
 }
 
 // Main class to demonstrate generic encapsulation
+public class GenericEncapsulationDemo {
+    public static void main(String[] args) {
+        // Creating an object for Integer type
+        DataHolder<Integer> intHolder = new DataHolder<>(100);
+        System.out.println("Integer Value: " + intHolder.getData());
 
+        // Updating the value
+        intHolder.setData(200);
+        System.out.println("Updated Integer Value: " + intHolder.getData());
+
+        // Creating an object for String type
+        DataHolder<String> stringHolder = new DataHolder<>("Hello, Generics!");
+        System.out.println("String Value: " + stringHolder.getData());
+
+        // Updating the value
+        stringHolder.setData("Updated String");
+        System.out.println("Updated String Value: " + stringHolder.getData());
+    }
+}
