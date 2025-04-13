2a5fd571e60d8c3b957d8eb3063d12a94747bf30
Sameer <bhandarisameer173@gmail.com>
2025-04-13 20:04:08 +0545

    ABC

--- ATMLogin.java
+++ ATMLogin.java
@@ -8,4 +8,24 @@ public class ATMLogin {
         String userInput;
         Scanner scanner = new Scanner(System.in);
 
-        
+        do {
+            System.out.print("Enter your 4-digit PIN: ");
+            userInput = scanner.nextLine();
+            attempts++;
+
+            if (userInput.equals(correctPin)) {
+                System.out.println("Access granted!");
+                break;
+            } else {
+                System.out.println("Incorrect PIN. Try again.");
+            }
+
+        } while (attempts < maxAttempts);
+
+        if (!userInput.equals(correctPin)) {
+            System.out.println("Account locked due to too many failed attempts.");
+        }
+
+        scanner.close();
+    }
+}
