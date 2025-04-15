2f2b800a58d7e03ab5e789a4c449f3fcd26c346a
Sameer <bhandarisameer173@gmail.com>
2025-04-15 21:31:42 +0545

    Random

---
+++ ButtonTextChanger.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ButtonTextChanger extends JFrame {
+    private JButton button;
+    private int clickCount = 0;
+
+    private final String[] messages = {
+        "Hello World",
+        "Hello Cutie",
+        "How are you"
+    };
+
+ 
+}
