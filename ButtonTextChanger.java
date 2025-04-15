6bc49a342dc09caf1d56de87a53517fbc6cfe3ec
Sameer <bhandarisameer173@gmail.com>
2025-04-15 21:31:51 +0545

    Random

--- ButtonTextChanger.java
+++ ButtonTextChanger.java
@@ -11,5 +11,26 @@ public class ButtonTextChanger extends JFrame {
         "How are you"
     };
 
- 
+    public ButtonTextChanger() {
+        setTitle("Button Text Changer");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button = new JButton(messages[0]); // Set initial text
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                clickCount = (clickCount + 1) % messages.length;
+                button.setText(messages[clickCount]);
+            }
+        });
+
+        add(button);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new ButtonTextChanger().setVisible(true);
+        });
+    }
 }
