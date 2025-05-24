d3744881b4687c64ff9207069b50f931b90e826d
Sameer <bhandarisameer173@gmail.com>
2025-05-24 22:28:06 +0545

    Do-while

--- ComplexDoWhileExample .java
+++ ComplexDoWhileExample .java
@@ -15,4 +15,22 @@ public class ComplexDoWhileExample {
             System.out.println("5. Exit");
             System.out.print("Enter your choice (1-5): ");
             
-       
+            choice = scanner.nextInt();
+            double a, b, result;
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter two numbers: ");
+                    a = scanner.nextDouble();
+                    b = scanner.nextDouble();
+                    result = a + b;
+                    System.out.println("Result: " + result);
+                    break;
+                case 2:
+                    System.out.print("Enter two numbers: ");
+                    a = scanner.nextDouble();
+                    b = scanner.nextDouble();
+                    result = a - b;
+                    System.out.println("Result: " + result);
+                    break;
+             
