5bcecef2e546901083ff55a76dbfdd9cca674992
Sameer <bhandarisameer173@gmail.com>
2025-05-26 21:19:47 +0545

    Ques

---
+++ MenuExample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MenuExample {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Say Hello");
+            System.out.println("2. Say Bye");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = input.nextInt();
+
+         
+        
+    }
+}
