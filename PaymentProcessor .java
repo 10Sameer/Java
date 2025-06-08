2527e1595e6f826f6f7d414e91d843cea9384713
Sameer <bhandarisameer173@gmail.com>
2025-06-08 20:41:08 +0545

    payment

--- PaymentProcessor .java
+++ PaymentProcessor .java
@@ -6,3 +6,17 @@ interface PaymentGateway {
 }
 
 // Implementation 1: Credit Card Payment
+class CreditCardPayment implements PaymentGateway {
+    public void authenticateUser() {
+        System.out.println("Authenticating Credit Card user via OTP...");
+    }
+
+    public void makePayment(double amount) {
+        System.out.println("Processing Credit Card payment of $" + amount);
+    }
+
+    public void generateReceipt() {
+        System.out.println("Receipt: Credit Card payment successful.\n");
+    }
+}
+
